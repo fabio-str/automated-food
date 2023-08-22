90f70062698537e4f85595a08a8c34c51a3763a9
fabio-str <fabio93straessle@hotmail.com>
2023-08-22 10:30:05 +0200

    created pending order mailer

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,15 @@
+class OrderMailer < ApplicationMailer
+    def new_pending_order(user, order)
+        @dish_name = order.dish_name
+        @ingredients = order.dish_ingredients
+        @total_calories = order.total_calories
+
+        @user = User.find(user.id) # set @user instance
+        @address = @user.address
+        @nutrition_profile = @user.nutrition_profile
+
+        mail(to: user.email, subject: 'Your daily lunch order')
+
+    end
+end
+  
